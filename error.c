282396d587b11c8d0c6d3a3a8c37ed3b04cd6c1b
silvia95 <ungureanu.sylvia@gmail.com>
2013-07-11 00:12:33 +0300

    added error functions

---
+++ error.c
@@ -0,0 +1,26 @@
+#include<error.h>
+#include<stdio.h>
+
+void errorOpeningSocket(){
+	printf("Error opening socket");
+}
+
+void errorOnBinding(){
+	printf("Error on binding");
+}
+
+void errorOnAccept(){
+	printf("Error on accept");
+}
+
+void errorWritingOnSocket(){
+	printf("Error writing on socket");
+}
+
+void errorConnecting(){
+	printf("Error connecting");
+}
+
+void noServerFound(){
+	printf("No server found");
+}
