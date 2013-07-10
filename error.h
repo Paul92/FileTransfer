57ead8bb25b3b637925fc4c17bbef787e5efd70f
Paul92 <paul.gafton@gmail.com>
2013-07-10 22:13:11 +0300

    Header for errors library

---
+++ error.h
@@ -0,0 +1,10 @@
+//SERVER ERRORS
+void errorOpeningSocket();
+void errorOnBinding();
+void errorOnAccept();
+
+//CLIENT ERRORS
+void errorOpeningSocket();
+void errorWritingOnSocket();
+void errorConnecting();
+void noServerFound();
